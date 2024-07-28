25cc0a1a8c5feb4909e0699470f86034264298d5
VAIBHAVKUMAR2004 <vk0101267@gmail.com>
2024-07-28 11:20:48 +0000

    datatypes

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,15 @@
+const accountId = 12823
+let accountEmail = "vaibhav2004@google.com"
+var accountPassword ="12345"
+accountCity ="bijnor"
+let accountState
+// accountId = 2  //not allowed
+
+accountEmail ="vk0101@gmail.com"
+accountCity ="delhi"
+accountPassword ="909090"
+
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treeat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+
+console.log("vaibhav")
+
+
+let name="vaibhav"
+let age ="20"
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint ---use in trading
+//string =>  ""
+//boolean => true/false
+// null => standalone value --type-object 
+// undefined =>             --type-undefine
+// symbol => unique
+
+
+// object =>
+
+    console.log(typeof null);      //object
+    console.log(typeof undefined); //undefine
