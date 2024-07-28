75b33539b5fa70529567b7e3d381863cc46ecd4e
VAIBHAVKUMAR2004 <vk0101267@gmail.com>
2024-07-28 16:01:23 +0000

    comarisionOperators

---
+++ 01-basics/04-comparision.js
@@ -0,0 +1,17 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+
+// console.log("02">1);
+// console.log("2">1);
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);  //diff property of comparision operators
+
+
