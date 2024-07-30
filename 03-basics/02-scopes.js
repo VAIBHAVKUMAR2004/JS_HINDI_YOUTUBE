50589264dc6c61329ad4db8940fc2e34b60e643e
VAIBHAVKUMAR2004 <vk0101267@gmail.com>
2024-07-30 11:58:22 +0000

    scope

---
+++ 03-basics/02-scopes.js
@@ -0,0 +1,14 @@
+
+var c =300                // global scope
+
+if (true) {                    // 
+let a = 10                     //local 
+    const b =20                 //scope
+    var c =30                    //
+}
+
+// console.log(a);
+
+// console.log(b);
+
+console.log(c);
