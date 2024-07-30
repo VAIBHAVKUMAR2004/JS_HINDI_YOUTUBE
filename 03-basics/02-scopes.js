fb64170fb9a0727567e30384cf493da1dc81ef4a
VAIBHAVKUMAR2004 <vk0101267@gmail.com>
2024-07-30 14:58:36 +0000

    scopes

--- 03-basics/02-scopes.js
+++ 03-basics/02-scopes.js
@@ -11,4 +11,46 @@ let a = 10                     //local
 
 // console.log(b);
 
-console.log(c);
+// console.log(c);
+
+
+function one(){
+    const username ="vaibhav"
+
+    function two(){
+        const website= "youtube"
+        console.log(username)
+    }
+    // console.log(website);
+
+
+    two()
+}
+
+one()
+
+if(true){
+    const username ="vaibhav"
+    if(username === "vaibhav"){
+      const website ="youtube"
+        console.log(username+website);
+    }
+   // console.log(website);
+   
+}
+// console.log(username);
+
+// +++++++++++++++++  interesting  +++++++++++++++++++
+
+console.log(addone(5));
+
+function addone(num){
+    return num+1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num+2
+}
