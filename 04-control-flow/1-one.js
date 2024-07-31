ead61d249fa2fde7e81ca64aed29e088020757b7
VAIBHAVKUMAR2004 <vk0101267@gmail.com>
2024-07-31 08:44:03 +0000

    control -flow

---
+++ 04-control-flow/1-one.js
@@ -0,0 +1,49 @@
+// if 
+// const isLoggedIn = true
+// const temp = 41
+
+// if(temp <50){
+// console.log("less than 50");
+// }
+// else{
+
+// console.log("temp is greater than 50")
+// }
+
+// const score =200
+
+// if(score>100){
+//     const power = "fly"
+// console.log(`User poewer:${power}`);
+// }
+// console.log(`User poewer:${power}`);
+
+
+const Balance = 1000
+
+// if(Balance >500) console.log("test");
+
+// if (Balance>500){
+// console.log("less than 500");
+
+// }  else if (Balance<750) {
+//     console.log("less than 750"); 
+
+// }  else if (Balance<950) {
+//     console.log("less than 950"); 
+
+// }  else {console.log("less than 1250");
+
+// }
+
+
+const userLoggedIn =  true 
+const debitCard = true
+
+if (userLoggedIn && debitCard){
+    console.log("Allow to buy course");
+}
+
+if(userLoggedIn || debitCard){
+    console.log("user logged in");
+}
